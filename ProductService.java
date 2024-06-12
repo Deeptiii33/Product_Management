4ac91431a6b453cce5650d6573138dbc110400dd
Deeptiii33 <141904236+Deeptiii33@users.noreply.github.com>
2024-06-13 02:10:30 +0530

    Add files via upload

---
+++ ProductService.java
@@ -0,0 +1,18 @@
+package com.pavan.service;
+
+import java.util.List;
+
+import com.pavan.model.Product;
+
+public interface ProductService {
+
+	public Product saveProduct(Product product);
+	
+	public List<Product> getAllProduct();
+	
+	public Product getProductById(Integer id);
+
+	public String deleteProduct(Integer id);
+	
+	public Product editProduct(Product product,Integer id);
+}
